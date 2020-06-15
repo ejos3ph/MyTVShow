b6a206ca8320f98af4632b71c6301a5e670f36be
ejos3ph <17875832+ejos3ph@users.noreply.github.com>
2020-06-15 00:29:51 -0600

    Added initial project files

---
+++ MyTVShow/Migrations/20200615041707_AddReleaseDate_ImageURL.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyTVShow.Migrations
+{
+    public partial class AddReleaseDate_ImageURL : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TvShow",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImdbUrl",
+                table: "TvShow",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "TvShow",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "TvShow",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "TvShow");
+
+            migrationBuilder.DropColumn(
+                name: "ReleaseDate",
+                table: "TvShow");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TvShow",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImdbUrl",
+                table: "TvShow",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
---
+++ MyTVShow/Models/Genre.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTVShow.Models
+{
+
+        public enum Genre
+        {
+            Drama,
+            Comedy,
+            Romance,
+            [Display(Name = "Romantic Comedy")]
+            RomCom,
+            Crime,
+            Mystery
+        }
+    
+}
